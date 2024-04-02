5edc0c98661c0c7ed7a6793b5c5d52cda99e1ba7
OJOJIN <ojy101039@gmail.com>
2024-04-02 11:46:46 +0900

    [init] project

---
+++ src/main/java/com/moldev/testproxydb/TestproxydbApplication.java
@@ -0,0 +1,13 @@
+package com.moldev.testproxydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestproxydbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestproxydbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moldev/testproxydb/TestproxydbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moldev.testproxydb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestproxydbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
